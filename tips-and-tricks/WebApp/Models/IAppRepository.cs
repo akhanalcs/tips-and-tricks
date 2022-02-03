b6430912337a0cd5fc9b310b68d6338404c2ea90
Ashish Khanal <AKhanal@marathonpetroleum.com>
2022-02-03 18:04:22 -0500

    Adds app repository interface

---
+++ tips-and-tricks/WebApp/Models/IAppRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public interface IAppRepository
+    {
+        IQueryable<Product> Products { get; }
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
+    }
+}
